3759570c9f2fbcd793a1bbf793f3e1b2a4048344
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-05-27 00:48:18 -0300

    Cleaning and reordering modules. We can now encode whatever .tif you throw.

--- src/huffman.py
+++ src/huffman.py
@@ -1,35 +1,19 @@
-# from itertools import batched # TO-DO: fix not working with current python version, +3.12 needed
-
+from itertools import batched 
 import queue
 
-def batched_manual(iterable, n):
-    # A simple manual implementation of batched
-    if n < 1:
-        raise ValueError('n must be at least one')
-    batch = []
-    for item in iterable:
-        batch.append(item)
-        if len(batch) == n:
-            yield tuple(batch)
-            batch = []
-    if batch:
-        yield tuple(batch)
-
-def build_huffman_tree(imarray, n=1):
-    # Count element frequencies
-    element_counts = {}
-    for element in batched_manual(imarray.flatten(), n):
-        if element in element_counts:
-            element_counts[element] += 1
+def build_huffman_tree(imarray, x=1):
+    histogram = {}
+    for element in batched(imarray.flatten(), x):
+        if element in histogram:
+            histogram[element] += 1
         else:
-            element_counts[element] = 1
+            histogram[element] = 1
 
     # Build priority queue to apply Huffman algorithm
     id = {}
     idr = {}
     q = queue.PriorityQueue()
-    for key, value in element_counts.items():
-        print(f"{key}, ")
+    for key, value in histogram.items():
         id[key] = len(id)
         idr[id[key]] = key
         q.put((value, [id[key]]))
@@ -57,10 +41,13 @@ def build_huffman_tree(imarray, n=1):
         transf[key] = path[::-1]
 
     # Build decoding dictionary mapping code string to original pixel tuple
-    code_to_pixel_tuple = {}
-    # Assuming 'id' maps pixel tuples to element IDs, and 'transf' maps element IDs to codes
+    tr = {} # Transform in reverse
     for pixel_tuple, element_id in id.items():
          code = transf[element_id]
-         code_to_pixel_tuple[code] = pixel_tuple
+         tr[code] = pixel_tuple
 
-    return transf, id, idr, code_to_pixel_tuple
+    cimarray = ''
+    for element in batched(imarray.flatten(), x):
+        cimarray += transf[id[element]]
+
+    return cimarray, tr
